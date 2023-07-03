d4e76f8edb26efef3b4283daa7cbb51b93745c54
Cocolennon <cocolennong@gmail.com>
2023-07-03 02:56:55 +0200

    Added custom cursor and made Windows default cursor invisible on webpage

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let grid = document.getElementById("grid");
 let gridHalfWidth = grid.offsetWidth / 2;
 
+let crs = document.getElementById("cursor");
+let crsHalfWidth = crs.offsetWidth / 2;
+
 document.onmousemove = function(e) {
     grid.style.left = e.pageX - gridHalfWidth + "px";
     grid.style.top = e.pageY - gridHalfWidth + "px";
+
+    crs.style.left = e.pageX - crsHalfWidth + "px";
+    crs.style.top = e.pageY - crsHalfWidth + "px";
 }
 
 document.addEventListener('contextmenu', event => event.preventDefault());
