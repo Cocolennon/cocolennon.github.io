e8d5a91f09d060ce81b81780a3ecb02d59f4e52f
Cocolennon <cocolennong@gmail.com>
2023-12-01 12:47:05 +0100

    Deleted all the files for the mobile page as it was very outdated

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-    window.location = "mobile";
-}
-
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 var lang = navigator.language.substring(0, 2);
